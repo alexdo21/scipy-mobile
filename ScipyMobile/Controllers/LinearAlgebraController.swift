ca1a627992a52b8723ac05c27a05d9f6ec8610f0
alexdo21 <alex.do21@yahoo.com>
2022-04-13 01:03:09 -0500

    added matrix editor

--- ScipyMobile/Controllers/LinearAlgebraController.swift
+++ ScipyMobile/Controllers/LinearAlgebraController.swift
@@ -6,10 +6,75 @@
 //
 
 import UIKit
+import MathEditor
 
 class LinearAlgebraController: ContentViewController {
+    let container: UIStackView = UIStackView()
+    let openBracket: UIView = {
+        let bracket = UIView()
+        bracket.addBorders(to: [.top, .left, .bottom], in: .black, width: 2)
+        return bracket
+    }()
+    let closeBracket: UIView = {
+        let bracket = UIView()
+        bracket.addBorders(to: [.top, .right, .bottom], in: .black, width: 2)
+        return bracket
+    }()
+    let matrixEditor: UIStackView = UIStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Linear Algebra"
+        title = "Linear Algebra | Determinant of Matrix"
+        
+        view.addSubview(container)
+
+        container.addArrangedSubview(openBracket)
+        container.addArrangedSubview(matrixEditor)
+        container.addArrangedSubview(closeBracket)
+        
+        container.distribution = .fillProportionally
+        container.spacing = 5
+        
+        openBracket.widthAnchor.constraint(equalToConstant: 10).isActive = true
+        closeBracket.widthAnchor.constraint(equalToConstant: 10).isActive = true
+
+        matrixEditor.axis = .vertical
+        matrixEditor.distribution = .fillEqually
+        matrixEditor.spacing = 15
+        for _ in 1...3 {
+            let row = UIStackView()
+            row.distribution = .fillEqually
+            row.spacing = 15
+            for _ in 1...3 {
+                let entry = UITextField()
+                entry.delegate = self
+                entry.textAlignment = .center
+                entry.text = "?"
+                entry.textColor = .white
+                entry.font = UIFont.systemFont(ofSize: 36)
+                entry.backgroundColor = UIColor.rgb(red: 173, green: 187, blue: 187)
+                row.addArrangedSubview(entry)
+            }
+            matrixEditor.addArrangedSubview(row)
+        }
+//        let row: UIStackView = matrixEditor.arrangedSubviews[1] as! UIStackView
+//        let entry: UITextField = row.arrangedSubviews[1] as! UITextField
+//        entry.text = "5"
+        container.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, bottom: nil, right: nil, padding: .init(top: 69, left: 20, bottom: 20, right: 20), size: .init(width: 203, height: 174))
     }
+}
+
+extension LinearAlgebraController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+//    func textFieldDidBeginEditing(_ textField: UITextField) -> Bool {
+//        textField.text = ""
+//        return true
+//    }
 }
--- ScipyMobile/Helpers/Extensions.swift
+++ ScipyMobile/Helpers/Extensions.swift
@@ -12,7 +12,7 @@ extension UIColor {
         return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
     }
     static func themeColor() -> UIColor {
-        return UIColor.rgb(red: 151, green: 196, blue: 196)
+        return UIColor.rgb(red: 151, green: 195, blue: 195)
     }
 }
 
@@ -49,6 +49,62 @@ extension UIView {
         if size.height != 0 {
             heightAnchor.constraint(equalToConstant: size.height).isActive = true
         }
+    }
+    
+    enum ViewSide {
+        case top
+        case left
+        case bottom
+        case right
+    }
+
+    func addBorders(to sides: [ViewSide], in color: UIColor, width: CGFloat) {
+        sides.forEach { addBorder(to: $0, in: color, width: width) }
+    }
+
+    func addBorder(to side: ViewSide, in color: UIColor, width: CGFloat) {
+        switch side {
+        case .top:
+            addTopBorder(in: color, width: width)
+        case .left:
+            addLeftBorder(in: color, width: width)
+        case .bottom:
+            addBottomBorder(in: color, width: width)
+        case .right:
+            addRightBorder(in: color, width: width)
+        }
+    }
+
+    func addTopBorder(in color: UIColor?, width borderWidth: CGFloat) {
+        let border = UIView()
+        border.backgroundColor = color
+        border.frame = CGRect(x: 0, y: 0, width: frame.size.width, height: borderWidth)
+        border.autoresizingMask = [.flexibleWidth, .flexibleBottomMargin]
+        addSubview(border)
+    }
+
+    func addBottomBorder(in color: UIColor?, width borderWidth: CGFloat) {
+        let border = UIView()
+        border.backgroundColor = color
+        border.frame = CGRect(x: 0, y: frame.size.height - borderWidth, width: frame.size.width, height: borderWidth)
+        border.autoresizingMask = [.flexibleWidth, .flexibleTopMargin]
+        addSubview(border)
+    }
+
+    func addLeftBorder(in color: UIColor?, width borderWidth: CGFloat) {
+        let border = UIView()
+        border.backgroundColor = color
+        border.frame = CGRect(x: 0, y: 0, width: borderWidth, height: frame.size.height)
+        border.autoresizingMask = [.flexibleHeight, .flexibleRightMargin]
+        addSubview(border)
+    }
+
+    func addRightBorder(in color: UIColor?, width borderWidth: CGFloat) {
+        let border = UIView()
+        border.backgroundColor = color
+        border.frame = CGRect(x: frame.size.width - borderWidth, y: 0, width: borderWidth, height: frame.size.height)
+        border.autoresizingMask = [.flexibleHeight, .flexibleLeftMargin]
+        addSubview(border)
     }
 }
 
